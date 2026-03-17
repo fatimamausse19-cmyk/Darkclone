340e39672da450b2d83f095adc1a50eb3e59e566
fatimamausse19-cmyk <fatimamausse19@gmail.com>
2026-03-17 23:32:56 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -4,9 +4,7 @@ API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 STRING_SESSION = os.environ["STRING_SESSION"]
 
-ORIGIN_CHAT = int(os.environ["ORIGIN_CHAT"])
-DEST_CHAT = int(os.environ["DEST_CHAT"])
-DEST_THREAD_ID = int(os.environ["DEST_THREAD_ID"])
+ORIGIN_CHAT = int(os.environ["ORIGIN_CHAT"])  # Canal ou grupo de origem
+DEST_CHAT = int(os.environ["DEST_CHAT"])      # Canal ou grupo de destino
 
-# Delay entre envios (em segundos)
-DELAY = float(os.environ.get("DELAY", 1))
+DELAY = float(os.environ.get("DELAY", 1))  # Delay entre envios em segundos
